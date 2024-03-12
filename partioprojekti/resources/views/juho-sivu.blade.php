43949c0c4810afee2f47a9089319bae40331ced1
VJuho <Juho.Vitikainen@student.lab.fi>
2024-03-12 15:02:53 +0200

    jännää
    sovellus

---
+++ partioprojekti/resources/views/juho-sivu.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Juhon kamaa') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="juho_1">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                No heiiiiih
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+</x-app-layout>
