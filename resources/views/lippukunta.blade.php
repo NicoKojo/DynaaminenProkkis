bd5054c11913707239ae6d953ade7cc4f76e2cf0
vilikosonen <116266208+vilikosonen@users.noreply.github.com>
2024-04-29 15:57:45 +0300

    Update lippukunta.blade.php
    Nyt on lisätty lippukunta sivulle linkit.

--- resources/views/lippukunta.blade.php
+++ resources/views/lippukunta.blade.php
@@ -12,6 +12,34 @@
 
 <div class="linkki"> <a href="https://www.partio.fi/lippukunta/oulun-metsankavijat-ry/">Oulun Metsänkävijät Ry </a></div> 
 
+<div class="linkki"> <a href="https://www.ensonpartiosiskot.net/">Enson Partiosiskot ry </a></div> 
+
+
+<div class="linkki"> <a href="https://korvenvaeltajat.net/">Korvenvaeltajat </a></div> 
+
+<div class="linkki"> <a href="https://lappeensiniset.net/">Lappeen Siniset ry </a></div> 
+
+<div class="linkki"> <a href="https://sites.google.com/site/lappeentuulitytot/">Lappeen tulitytöt ry </a></div> 
+
+<div class="linkki"> <a href="https://wallisirkut.wordpress.com/">Lappeenrannan wallisirkut </a></div> 
+
+<div class="linkki"> <a href="https://walliveljet.net/">walliveljet </a></div> 
+
+<div class="linkki"> <a href="https://www.vuorenveikot.net/">Vuorenveikot </a></div> 
+
+<div class="linkki"> <a href="https://taavetin-korpihaukat.webnode.fi/">Taavetin Korpihaukat </a></div>
+
+<div class="linkki"> <a href="https://tarmontytot.partioscout.fi/">Tarmontytöt </a></div>
+
+<div class="linkki"> <a href="https://liekkitytot.fi/">Liekkitytöt </a></div>
+
+
+
+
+
+
+
+
 
 
 </x-app-layout>
