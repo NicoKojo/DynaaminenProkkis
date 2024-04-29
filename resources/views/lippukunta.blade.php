c6997f70bfa681f37098f264b11e55236a3a00ad
vilikosonen <116266208+vilikosonen@users.noreply.github.com>
2024-04-29 18:32:30 +0300

    Päivitys lippukuntaan
    lippukunta sivua muokattu.

--- resources/views/lippukunta.blade.php
+++ resources/views/lippukunta.blade.php
@@ -10,28 +10,30 @@
 </div>
 
 
-<div class="linkki"> <a href="https://www.partio.fi/lippukunta/oulun-metsankavijat-ry/">Oulun Metsänkävijät Ry </a></div> 
-
-<div class="linkki"> <a href="https://www.ensonpartiosiskot.net/">Enson Partiosiskot ry </a></div> 
 
 
-<div class="linkki"> <a href="https://korvenvaeltajat.net/">Korvenvaeltajat </a></div> 
+<div class="linkki"> <a href="https://www.ensonpartiosiskot.net/" target="_blank">Enson Partiosiskot ry </a></div> 
 
-<div class="linkki"> <a href="https://lappeensiniset.net/">Lappeen Siniset ry </a></div> 
 
-<div class="linkki"> <a href="https://sites.google.com/site/lappeentuulitytot/">Lappeen tulitytöt ry </a></div> 
+<div class="linkki"> <a href="https://korvenvaeltajat.net/"target="_blank">Korvenvaeltajat </a></div> 
 
-<div class="linkki"> <a href="https://wallisirkut.wordpress.com/">Lappeenrannan wallisirkut </a></div> 
+<div class="linkki"> <a href="https://lappeensiniset.net/"target="_blank">Lappeen Siniset ry </a></div> 
 
-<div class="linkki"> <a href="https://walliveljet.net/">walliveljet </a></div> 
+<div class="linkki"> <a href="https://sites.google.com/site/lappeentuulitytot/"target="_blank">Lappeen tulitytöt ry </a></div> 
 
-<div class="linkki"> <a href="https://www.vuorenveikot.net/">Vuorenveikot </a></div> 
+<div class="linkki"> <a href="https://wallisirkut.wordpress.com/"target="_blank">Lappeenrannan wallisirkut </a></div> 
 
-<div class="linkki"> <a href="https://taavetin-korpihaukat.webnode.fi/">Taavetin Korpihaukat </a></div>
+<div class="linkki"> <a href="https://walliveljet.net/"target="_blank">walliveljet </a></div> 
 
-<div class="linkki"> <a href="https://tarmontytot.partioscout.fi/">Tarmontytöt </a></div>
+<div class="linkki"> <a href="https://www.vuorenveikot.net/"target="_blank">Vuorenveikot </a></div> 
 
-<div class="linkki"> <a href="https://liekkitytot.fi/">Liekkitytöt </a></div>
+<div class="linkki"> <a href="https://taavetin-korpihaukat.webnode.fi/"target="_blank">Taavetin Korpihaukat </a></div>
+
+<div class="linkki"> <a href="https://tarmontytot.partioscout.fi/"target="_blank">Tarmontytöt </a></div>
+
+<div class="linkki"> <a href="https://liekkitytot.fi/"target="_blank">Liekkitytöt </a></div>
+
+<div class="linkki1"> <a href="https://www.partio.fi/lippukunta/oulun-metsankavijat-ry/"target="_blank">Oulun Metsänkävijät Ry </a></div> 
 
 
 
